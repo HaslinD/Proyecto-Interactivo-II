3287bb52e1370e13154964ff7cdef84b2975d422
Jsac2000 <61567116+Jsac2000@users.noreply.github.com>
2021-09-11 11:40:11 -0600

    Incorporacion Modal de NUEVA PUBLICACION
    -Se agrego codigo de modal en main.html
    -Cambios en styles_main.css
    -Enlace a main.js en codigo de main.html
    -Programacion minima en main.js
    -Se agregaron imgs ejemplo1, ejemplo2

--- js/main.js
+++ js/main.js
@@ -11,3 +11,42 @@ $(function() {
 	});
 
 });
+
+
+
+
+/*-------------NUEVA PUBLICACION POP UP------------*/
+
+/*   Primer Intento
+function myFunction() {
+  var popup = document.getElementById("ventanaPublicar");
+  .toggleClass("show");
+}
+*/
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("publicacionBTN");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("cerrar")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+/*-------------FIN NUEVA PUBLICACION POP UP------------*/
