93873dd8f88b9d9af5aa8956e1380fa0068b98f1
Jsac2000 <61567116+Jsac2000@users.noreply.github.com>
2021-09-21 20:53:18 -0600

    Adiciones de Nueva Publicación
    -X  para cerrar modal
    -Parrafo de seleccionar archivos
    -Emotes

--- js/main.js
+++ js/main.js
@@ -30,8 +30,11 @@ var modal = document.getElementById("myModal");
 // Get the button that opens the modal
 var btn = document.getElementById("publicacionBTN");
 
+// Get the <input> element that closes the modal
+var input = document.getElementsByClassName("cerrar")[0];
+
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("cerrar")[0];
+var span = document.getElementsByClassName("xCerrar")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
@@ -40,6 +43,11 @@ btn.onclick = function() {
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+input.onclick = function() {
   modal.style.display = "none";
 }
 
